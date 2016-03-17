7f24b657b1a1c205de5811a478c9da24ce9069a9
Muhammad Hijazi <irekasoft@gmail.com>
2016-03-17 17:38:21 +0800

    Initial Commit

---
+++ RealmLoadFile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RealmLoadFile
+//
+//  Created by Hijazi on 17/3/16.
+//  Copyright © 2016 iReka Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
